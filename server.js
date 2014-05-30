1f3ade86f6b560e787d036e577ad0b39b96ccded
senthilnathan <smani@sapient.com>
2014-05-30 11:00:04 +0530

    small test

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ var delivery = dl.listen(socket);
 			});
 			var index = pseudoArray.indexOf(pseudo);
 			pseudoArray.slice(index);
-			io.sockets.emit('userslist', {"ulist": pseudoArray});
+			//io.sockets.emit('userslist', {"ulist": pseudoArray});
 		}
 	});
 });
